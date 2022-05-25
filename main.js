7ac949979585a1f0f5c118b7afd7ec15635d0972
Guilherme Gomes <guilherme.gsm96@gmail.com>
2022-05-25 20:03:50 -0300

    master

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'Guilhermegsm',
+  youtube: 'UCr96JY9A7YzyLOs0T4YHmnA',
+  facebook: 'guilherme.gomes.3950',
+  instagram: 'guilhermeogomes',
+  twitter: 'guilhermegsm96'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
